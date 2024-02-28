0af841f96ae870f524fb30515cf0e78e3dea124f
Enrique <enrique.sousa@gmail.com>
2024-02-27 17:42:26 -0800

    S12 87. Frontend Footer Show Setup

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -1,3 +1,7 @@
+@php
+    $dataFooter = App\Models\Footer::find(1);
+@endphp
+
 <!-- Footer-area -->
 <footer class="footer">
     <div class="container">
@@ -5,42 +9,44 @@
             <div class="col-lg-4">
                 <div class="footer__widget">
                     <div class="fw-title">
-                        <h5 class="sub-title">Contact us</h5>
-                        <h4 class="title">+81383 766 284</h4>
+                        <h5 class="sub-title">Contactarnos</h5>
+                        <h4 class="title">{{ $dataFooter->number }}</h4>
                     </div>
                     <div class="footer__widget__text">
-                        <p>There are many variations of passages of lorem ipsum
-                        available but the majority have suffered alteration
-                        in some form is also here.</p>
+                        <p>{{ $dataFooter->short_description }}</p>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-lg-4 col-sm-6">
                 <div class="footer__widget">
                     <div class="fw-title">
-                        <h5 class="sub-title">my address</h5>
-                        <h4 class="title">AUSTRALIA</h4>
+                        <h5 class="sub-title">mi dirección</h5>
+                        <h4 class="title">Tijuana BC</h4>
                     </div>
                     <div class="footer__widget__address">
-                        <p>Level 13, 2 Elizabeth Steereyt set <br> Melbourne, Victoria 3000</p>
-                        <a href="mailto:noreply@envato.com" class="mail">noreply@envato.com</a>
+                        {{-- <p>Level 13, 2 Elizabeth Steereyt set <br> Melbourne, Victoria 3000</p> --}}
+                        <p>{{ $dataFooter->address }}</p>
+                        <a href="mailto:noreply@envato.com" class="mail">{{ $dataFooter->email }}</a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-3 col-lg-4 col-sm-6">
                 <div class="footer__widget">
                     <div class="fw-title">
-                        <h5 class="sub-title">Follow me</h5>
-                        <h4 class="title">socially connect</h4>
+                        <h5 class="sub-title">Sígueme en</h5>
+                        <h4 class="title">redes sociales</h4>
                     </div>
                     <div class="footer__widget__social">
                         <p>Lorem ipsum dolor sit amet enim. <br> Etiam ullamcorper.</p>
                         <ul class="footer__social__list">
-                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+
+                            <li><a href="{{ $dataFooter->facebook }}" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="{{ $dataFooter->twitter }}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+
                             <li><a href="#"><i class="fab fa-behance"></i></a></li>
                             <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
                             <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+
                         </ul>
                     </div>
                 </div>
@@ -50,7 +56,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="copyright__text text-center">
-                        <p>Copyright @ Theme_Pure 2021 All right Reserved</p>
+                        <p>{{ $dataFooter->copyright }}</p>
                     </div>
                 </div>
             </div>
